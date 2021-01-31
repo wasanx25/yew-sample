aafb6129331465c8bc46f131674b918005b7b791
wasanx25 <wasanx25@gmail.com>
2021-01-31 21:12:42 +0900

    Add counter sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use yew::{html, Component, ComponentLink, Html};
+
+pub enum Msg {
+    Increment,
+    Decrement,
+}
+
+pub struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { link, value: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Increment => {
+                self.value += 1;
+                true
+            }
+            Msg::Decrement => {
+                self.value -= 1;
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> bool {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::Increment)>
+                    { "Increment" }
+                </button>
+                <button onclick=self.link.callback(|_| Msg::Decrement)>
+                    { "Decrement" }
+                </button>
+
+                <p>{ "Current Value: " } <strong>{ self.value }</strong></p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
