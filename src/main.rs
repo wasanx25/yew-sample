b10d0cc0e579b9bb5e06165399b46e4a4093c1a3
wasanx25 <wasanx25@gmail.com>
2021-02-04 01:14:58 +0900

    fetch

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
-use yew::{html, Component, ComponentLink, Html};
+use anyhow::Error;
+use serde_derive::{Deserialize};
+use yew::{Component, ComponentLink, html, Html};
+use yew::format::{Json, Nothing};
+use yew::services::fetch::*;
 
 pub enum Msg {
-    Increment,
-    Decrement,
+    FetchData,
+    FetchReady(Result<Vec<User>, Error>),
+    Ignore,
 }
 
 pub struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    data: Vec<User>,
+    fetch_task: Option<FetchTask>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct User {
+    id: i64,
+    name: String,
+    username: String,
+    email: String,
+    website: String,
+}
+
+impl Model {
+    fn fetch(&mut self) -> FetchTask {
+        let callback = self.link.callback(
+            move |response: Response<Json<Result<Vec<User>, Error>>>| {
+                let (meta, Json(data)) = response.into_parts();
+                // println!("META: {:?}, {:?}", meta, data);
+                if meta.status.is_success() {
+                    Msg::FetchReady(data)
+                } else {
+                    Msg::Ignore
+                }
+            },
+        );
+
+        let request = Request::get("https://jsonplaceholder.typicode.com/users").body(Nothing).unwrap();
+        FetchService::fetch(request, callback).unwrap()
+    }
 }
 
 impl Component for Model {
@@ -15,18 +49,22 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self { link, data: Vec::new(), fetch_task: None }
     }
 
     fn update(&mut self, msg: Self::Message) -> bool {
         match msg {
-            Msg::Increment => {
-                self.value += 1;
+            Msg::FetchData => {
+                let task = self.fetch();
+                self.fetch_task = Some(task);
                 true
             }
-            Msg::Decrement => {
-                self.value -= 1;
+            Msg::FetchReady(response) => {
+                self.data = response.ok().unwrap();
                 true
+            }
+            Msg::Ignore => {
+                false
             }
         }
     }
@@ -38,14 +76,19 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::Increment)>
-                    { "Increment" }
-                </button>
-                <button onclick=self.link.callback(|_| Msg::Decrement)>
-                    { "Decrement" }
+                <button onclick=self.link.callback(|_| Msg::FetchData)>
+                    { "Hoge" }
                 </button>
 
-                <p>{ "Current Value: " } <strong>{ self.value }</strong></p>
+                <ul>
+                    { for self.data.iter().map(|user| {
+                        html! {
+                            <li>
+                                { user.username.to_string() }
+                            </li>
+                        }
+                    })}
+                </ul>
             </div>
         }
     }
